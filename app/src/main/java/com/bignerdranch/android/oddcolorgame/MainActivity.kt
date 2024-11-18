d43a4aca2a860a0264d2f42beaee1bc032c40f6a
bruhsselssprout <158619756+bruhsselssprout@users.noreply.github.com>
2024-11-18 13:59:55 -0800

    Added WIP Title Screen
    WIP title screen

--- app/src/main/java/com/bignerdranch/android/oddcolorgame/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/oddcolorgame/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.bignerdranch.android.oddcolorgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Start Screen -> Game
+        val startGameButton = findViewById<Button>(R.id.startGame)
+        startGameButton.setOnClickListener {
+            val startToGame = Intent(this, TestGameScreen::class.java)
+            startActivity(startToGame)
+        }
     }
 }
