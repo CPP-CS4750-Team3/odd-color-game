3eacc0e7a5d42c77aeaa0560dd35a0de6e5eec0f
bruhsselssprout <158619756+bruhsselssprout@users.noreply.github.com>
2024-11-18 17:24:27 -0800

    Added Animated BG

--- app/src/main/java/com/bignerdranch/android/oddcolorgame/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/oddcolorgame/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.bignerdranch.android.oddcolorgame
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Make background animated
+        val titleScreenBg = findViewById<ConstraintLayout>(R.id.main_layout)
+        val bgAnimation: AnimationDrawable = titleScreenBg.background as AnimationDrawable
+        bgAnimation.setEnterFadeDuration(1000)
+        bgAnimation.setExitFadeDuration(1000)
+        bgAnimation.start()
 
         //Start Screen -> Game
         val startGameButton = findViewById<Button>(R.id.startGame)
