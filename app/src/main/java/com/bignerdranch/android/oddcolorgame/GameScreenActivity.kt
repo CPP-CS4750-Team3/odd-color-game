f18c98a3da89753a645b0d90d2c7545bc8fdbe4d
ragod02 <raygod02@gmail.com>
2024-12-01 18:52:26 -0800

    2nd commit

--- app/src/main/java/com/bignerdranch/android/oddcolorgame/GameScreenActivity.kt
+++ app/src/main/java/com/bignerdranch/android/oddcolorgame/GameScreenActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.oddcolorgame
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +23,8 @@ class GameScreenActivity : AppCompatActivity(), SquareAdapter.SquareClickListene
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewGrid)
         recyclerView.layoutManager = GridLayoutManager(this, gameDifficulty.columns) // 4 columns in the grid
 
+        findViewById<TextView>(R.id.game_difficulty).text = "Difficulty: ${gameDifficulty.name}"
+
         // Initialize adapter with initial data (e.g., 16 squares with one being different)
         val initialSquares = MutableList(numOfSquares) { false } // First square is different
         val differentSquareIndex = (0 until numOfSquares).random()
@@ -29,11 +32,13 @@ class GameScreenActivity : AppCompatActivity(), SquareAdapter.SquareClickListene
 
         squareAdapter = SquareAdapter(initialSquares, gameDifficulty, score, this)
         recyclerView.adapter = squareAdapter
+
+        updateScoreAndStage()
     }
 
     override fun onSquareClick(position: Int) {
         val clickedSquareIsDifferent = squareAdapter.squares[position]
-        if (clickedSquareIsDifferent) { updateSquares(); score++ }
+        if (clickedSquareIsDifferent) { score++; updateScoreAndStage(); updateSquares() }
     }
 
     private fun updateSquares() {
@@ -41,5 +46,12 @@ class GameScreenActivity : AppCompatActivity(), SquareAdapter.SquareClickListene
         val differentSquareIndex = (0 until numOfSquares).random()
         newSquares[differentSquareIndex] = true
         squareAdapter.updateSquares(newSquares, score)
+    }
+
+    private fun updateScoreAndStage() {
+        findViewById<TextView>(R.id.game_score).text = "Score: $score"
+
+        val stage = score / 6 + 1
+        findViewById<TextView>(R.id.game_stage).text = "Stage: $stage"
     }
 }
